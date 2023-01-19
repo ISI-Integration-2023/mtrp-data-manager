7154e565f71c01905d5f5f4d65b2735d92ccf380
adityaruplaha <bs2124@isical.ac.in>
2023-01-19 23:25:09 +0530

    Start broadcasting brochure.

--- brochure_mailer/send_emails.py
+++ brochure_mailer/send_emails.py
@@ -1,32 +1,36 @@
 import os
-import pandas as pd
+import csv
 
 from multiprocessing import Pool
 
-import email
+from email import message_from_binary_file
+from email.headerregistry import Address
 import smtplib
 
-creds = json.load(open("admit_mailer/credentials.json"))
-msg = email.message_from_binary_file(open(f"brochure_mailer/msg_to_schools.eml", "rb"))
+msg = message_from_binary_file(open(f"brochure_mailer/msg_to_schools.eml", "rb"))
 
-def send_email(name : str, email : str):
+def send_email(name : str, email_id : str):
+    name.replace(',', ' ').replace('\n', ' ')
     with smtplib.SMTP("mail.isical.ac.in", 500) as smtp:
-        msg["To"] = email.headerregistry.Address(name, *(email.lower().split('@')[:2]))
+        msg['To'] = f"{name} <{email_id}>"
         smtp.send_message(msg)
-        print(f"BROCHURE MAILER: Sent brochure to {name} <{email}>.")
+        print(f"BROCHURE MAILER: Sent brochure to {name} <{email_id}>.")
         with open("brochure_mailer/sent.txt", "a") as sent:
-            sent.write(email + "\n")
+            sent.write(email_id + "\n")
 
 def run():
     sent = [email.strip() for email in open("brochure_mailer/sent.txt").readlines()]
-    data = [(name, email) for name, email in pd.read_csv("brochure_mailer/schools.csv").dropna(how='any').to_dict().items() if email not in sent][:50]
-    print(f"BROCHURE MAILER: Sending for the following adresses... ({len(data)})")
-    print("-"*140)
-    for name, email in data:
-        print(f"{name} \t\t<{email}>")
-    print("-"*140)
-    inp = input("Proceed? (y/N): ").lower()
-    print("-"*140)
-    if len(inp) >= 1 and inp[0] == 'y':
-        with Pool(10) as pool:
-            pool.starmap(send_email, data)
+    with open("brochure_mailer/schools.csv") as f:
+        reader = csv.DictReader(f)
+        data = [(row["name"], row["email"]) for row in reader if row["name"] and row["email"] and row["email"] not in sent][:50]
+        print(f"BROCHURE MAILER: Sending for the following adresses... ({len(data)})")
+        print("-"*140)
+        for name, email in data:
+            name.replace(',', ' ').replace('\n', ' ')
+            print(f"{name:30} <{email}>")
+        print("-"*140)
+        inp = input("Proceed? (y/N): ").lower()
+        print("-"*140)
+        if len(inp) >= 1 and inp[0] == 'y':
+            with Pool(10) as pool:
+                pool.starmap(send_email, data)
