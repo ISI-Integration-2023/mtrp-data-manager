102abd8fb9ec4525323b75f4d7f865099490fb07
adityaruplaha <bs2124@isical.ac.in>
2023-02-03 20:50:18 +0530

    Allocator fixes.

--- classroom_allocation/allocator.py
+++ classroom_allocation/allocator.py
@@ -14,7 +14,7 @@ def allocate():
     existing["email"] = existing["email"].str.lower().str.strip()
     existing.drop_duplicates(subset=["reg_no", "name", "email"], inplace=True, ignore_index=True)
     e_list = existing["email"].to_list()
-    existing = existing.filter(["email", "classroom"]).join(df.set_index("email"), on="email")
+    existing = existing.filter(["email", "category", "classroom"]).join(df.set_index(["email", "category"]), on=["email", "category"])
     df["zone"] = df["zone"].map(lambda e: e if e == "Online" else None)
     df["email"] = df["email"].str.lower().map(lambda e: e if email_re.match(e) and e.endswith("@gmail.com") else None, na_action='ignore')
     df["email"] = df["email"][~ df["email"].isin(e_list)]
@@ -27,6 +27,6 @@ def allocate():
         j["classroom"] = f"J{(i+1):02}"
     for i, s in enumerate(dfs_senior):
         s["classroom"] = f"S{(i+1):02}"
-    df = pd.concat([existing] + dfs_junior + dfs_senior, ignore_index=True).filter(["reg_no", "name", "email", "contact", "classroom", "medium"])
+    df = pd.concat([existing] + dfs_junior + dfs_senior, ignore_index=True).filter(["reg_no", "name", "email", "contact", "alt_contact", "category", "classroom", "medium"])
     df.drop_duplicates(subset=["reg_no", "name", "email"], inplace=True, ignore_index=True)
     df.to_csv("classroom_allocation/allocation.csv", index=False)
